306eda3c155e18b6d716985c815bd4016c908a03
momen samer <momensamer807@gmail.com>
2025-07-28 15:56:26 +0000

    new update

--- models/marts/customersegmintation.sql
+++ models/marts/customersegmintation.sql
@@ -1,22 +1,21 @@
-with customer_sales AS(
-    select 
-    s.customer_id,
-    SUM(S.quantity_sold) AS total_purchases,
-    SUM(s.total_amount) AS total_spend,
- from {{ref("stg_retail__fact_sales")}} s,
- group by s.customer_id 
-    
-
+WITH customer_sales AS (
+   SELECT
+       s.customer_id,
+       sum(s.quantity_sold) AS total_purchases,
+       SUM(s.total_amount) AS total_spend
+   FROM {{ ref('stg_retail__fact_sales') }} s
+   GROUP BY s.customer_id
 )
 
-select 
-cs.customer_id,
-cs.total_purchases,
-cs.total_spend,
-CASE
-    WHEN cs.total_purchases >= 50 THEN 'high_value'
-    WHEN cs.total_purchases BETWEEN 20 AND 49 THEN 'medium_value'
-    WHEN cs.total_purchases <= 20 THEN 'low_value'
-    ELSE 'unknown'
-END AS customer_segment
+
+SELECT
+   cs.customer_id,
+   cs.total_purchases,
+   cs.total_spend,
+   CASE
+       WHEN cs.total_purchases >= 50  THEN 'High-Value'
+       WHEN cs.total_purchases BETWEEN 20 AND 49 THEN 'Medium-Value'
+       WHEN cs.total_purchases < 20  THEN 'Low-Value'
+       ELSE 'Unknown'
+   END AS customer_segment
 FROM customer_sales cs
